c5b9471c271116fb2f285e796a877c0b8e29eea0
kila <siwipriskilaa@gmail.com>
2026-06-22 20:11:28 +0700

    Upload Tugas Sesi 10

--- test/test-sauce2.js
+++ test/test-sauce2.js
@@ -5,7 +5,7 @@ const {Select} = require('selenium-webdriver/lib/select');
 describe ('Google Search Test', function () {
     let driver;
 
-    it('Visit SauceDemo dan cek page title', async function (){
+    before(async function () {
         driver = await new Builder().forBrowser('chrome').build();
 
         await driver.get('https://www.saucedemo.com');
@@ -14,10 +14,15 @@ describe ('Google Search Test', function () {
         let inputPassword = await driver.findElement(By.xpath('//*[@data-test="password"]') )
         let buttonLogin = await driver.findElement(By.className("submit-button btn_action"))
         await inputUsername.sendKeys("standard_user")
-        await driver.sleep(500);
         await inputPassword.sendKeys("secret_sauce")
-        await driver.sleep(500);
         await buttonLogin.click()
+    })
+
+    after(async function () {
+        await driver.quit();
+    })
+
+    it('Pilih Dropdown Z to A', async function (){
 
         let title = await driver.getTitle()
         assert.strictEqual(title, 'Swag Labs');
@@ -39,9 +44,23 @@ describe ('Google Search Test', function () {
         let select = new Select(dropdownSort);
         await select.selectByVisibleText('Name (Z to A)');
         await driver.sleep(1000);
+                 
+    })
 
-        await driver.quit();
-          
+     it('Pilih Price (low to high)', async function (){
+
+        const actions = driver.actions({async: true});
+        let dropdownSort = await driver.findElement(By.xpath('//select[@data-test="product-sort-container"]'));
+        
+        await dropdownSort.click();
+        await actions.move({origin: dropdownSort}).perform();  // kursor gerak ke dropdown
+        await driver.sleep(1000);
+
+        let select = new Select(dropdownSort);
+        await select.selectByVisibleText('Price (low to high)');
+        await driver.sleep(1000);
+                 
     })
     
 })
+
